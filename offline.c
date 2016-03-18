735f9277497e571225073f43daec0d02a1671bd3
P0ppoff <jules.hablot@gmail.com>
2016-03-18 12:46:18 +0100

    step one

--- offline.c
+++ offline.c
@@ -6,45 +6,59 @@
 
 #include "rdjpeg.h"
 
+#define NB_INTER 4
+#define NIVEAU_COLOR 256
+#define TAILLE_HISTO NB_INTER * NB_INTER * NB_INTER
+
+void make_histo (float *hist, CIMAGE *image){
+	int R, G, B, k, i, j;
+	for(i=0 ; i<image->nx ; i++){
+		for(j=0 ; j<image->ny ; j++){
+			R = image->r[i][j] / (NIVEAU_COLOR/NB_INTER);
+			G = image->g[i][j] / (NIVEAU_COLOR/NB_INTER);
+			B = image->b[i][j] / (NIVEAU_COLOR/NB_INTER);
+			k = R + G*4 + B*16;
+			hist[k]++;
+		}
+	}	
+}
+
+void normalize (float *hist, CIMAGE *image){
+	int i;
+	float nb_pixel = image->ny * image->nx;
+	for(i=0 ; i<TAILLE_HISTO ; i++){
+		hist[i] = hist[i]/nb_pixel;
+	}
+}
+
+void init_tab (float *tab, int taille){
+	int i;
+	for(i=0; i<taille ; i++){
+		tab[i] = 0.0;
+	}
+}
+
+void print_histo (float *hist){
+	int i;
+	float sum = 0.0;
+	for(i=0 ; i<TAILLE_HISTO ; i++){
+		printf("[%i] -> %f\n", i, hist[i]);
+		sum += hist[i];
+	}
+	printf("Sum : %f\n", sum);
+}
+
 int main(int argc, char *argv[])
 {
-  int i,j,n,nx,ny,nb;
-  CIMAGE cim;
-  /*------------------------------------------------*/
-  /* lecture d'une image requête                    */
-  /*------------------------------------------------*/
-  read_cimage(argv[1],&cim);
-  /*------------------------------------------------*/
-  /* affichage des valeurs pour le premier bloc 8x8 */
-  /* comme exemple de traitement                    */
-  /*------------------------------------------------*/
-  printf("Largeur de l'image : %d\n",cim.nx);
-  printf("Heuteur de l'image : %d\n",cim.ny);
-  printf("Plan rouge du premier bloc 8x8 :\n");
-  for (j = 0; j < 8; j++) {       /* ligne par ligne */
-    printf("  ");
-    for (i = 0; i < 8; i++) {   /* pixel par pixel */
-      printf("%4d",cim.r[i][j]);
-    }
-    printf("\n");
-  }
-  printf("Plan vert du premier bloc 8x8 :\n");
-  for (j = 0; j < 8; j++) {       /* ligne par ligne */
-    printf("  ");
-    for (i = 0; i < 8; i++) {   /* pixel par pixel */
-      printf("%4d",cim.g[i][j]);
-    }
-    printf("\n");
-  }
-  printf("Plan bleu du premier bloc 8x8 :\n");
-  for (j = 0; j < 8; j++) {       /* ligne par ligne */
-    printf("  ");
-    for (i = 0; i < 8; i++) {   /* pixel par pixel */
-      printf("%4d",cim.b[i][j]);
-    }
-    printf("\n");
-  }
-  /*------------------------------------------------*/
-
-  exit(0);
+	int i,j;
+	CIMAGE cim;
+	float histogramme_cubique[TAILLE_HISTO];
+	
+	read_cimage(argv[1],&cim);
+	init_tab(histogramme_cubique, TAILLE_HISTO);
+	make_histo(histogramme_cubique, &cim);
+	normalize(histogramme_cubique, &cim);
+	print_histo(histogramme_cubique);
+	
+	exit(0);
 }
