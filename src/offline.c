4459c9470e3809534fba07cffc0d9608202b4613
Gorgorot38 <gorgorot38@gmail.com>
2016-03-23 16:33:59 +0100

    multi histogramme

--- src/offline.c
+++ src/offline.c
@@ -5,6 +5,7 @@
 #include <strings.h>
 
 #include "rdjpeg.h"
+#include "proc.h"
 
 #define NB_INTER 4
 #define NIVEAU_COLOR 256
@@ -51,16 +52,32 @@ void print_histo (float *hist){
 void traitement (char *name){
 	CIMAGE cim;
 	float histogramme_cubique[TAILLE_HISTO];
+
+	printf("Traitement %s\n", name);
 	
 	read_cimage(name, &cim);
 	init_tab(histogramme_cubique, TAILLE_HISTO);
 	make_histo(histogramme_cubique, &cim);
 	normalize(histogramme_cubique, &cim);
 	print_histo(histogramme_cubique);
+	free_cimage(NULL, &cim);
+}
+
+void traitement_multi (char *filename){
+
+	int i;
+	int nb_images;
+	char **list_file_name;
+	
+	list_file_name = readList(filename, &nb_images);
+	for (i=0; i<nb_images; i++){
+		traitement(list_file_name[i]);
+	}
+	freeList(list_file_name, nb_images);
 }
 
 int main(int argc, char *argv[])
 {
-	traitement(argv[1]);	
+	traitement_multi(argv[1]);	
 	exit(0);
 }
